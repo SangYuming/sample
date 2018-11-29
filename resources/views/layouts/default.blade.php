d20fe2cc48b82adec05e6d3cae3a3ccc26e7a2f6
SangYuming <18725093271@163.com>
2018-11-29 15:22:27 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title','Sample') - Laravel</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </body>
 </html>
